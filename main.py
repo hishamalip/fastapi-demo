8def6cd4ab408e5a7b935b15c767c49e773bd1fb
hishamalip <hishamalip@gmail.com>
2026-05-28 20:14:14 +0530

    added files

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from models import Product
+
+app = FastAPI()
+
+@app.get("/")
+def greet():
+    return "Welcome to FastAPI by Hisham"
+
+
+
+
+products = [
+    Product(id=1, name="phone", description="budget phone", price=99, quantity=10),
+    Product(id=2, name="laptop", description="gaming laptop", price=123.43, quantity=6),
+    Product(id=3, name="mouse", description="wireless mouse", price=52.5, quantity=4)
+]
+
+# get all products
+@app.get("/products/")
+def get_all_products():
+    return products
+
+
+# get a single product
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+        
+    return "product not found"
+
+
+# add product
+@app.post("/products/")
+def add_product(product: Product):
+    products.append(product)
+    return "product added successfully"
+
+
+# delete a product
+@app.delete("/products/{id}")
+def delete_product(id: int):
+    for product in products:
+        if product.id == id:
+            products.remove(product)
+            return f"product with id: {id} deleted successfull"
+    
+    return f"no product with id: {id} found"
+
+
+# update a product
+@app.put("/products/")
+def update_product(id: int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "product added successfully"
+    
+    return f"product with id: {id} not found"
